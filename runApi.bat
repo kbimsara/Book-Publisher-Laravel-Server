91c15cf834d254c8ed6c5cb55237ebacd54259f9
Kavindu Bimsara <34527634+kbimsara@users.noreply.github.com>
2023-10-17 22:26:55 +0530

    gg

---
+++ runApi.bat
@@ -0,0 +1,8 @@
+:: Created by HUNTER
+@echo off
+echo ############################################
+echo            Created by :- HUNTER
+echo ############################################
+cd Rest-api
+php artisan serve
+pause
